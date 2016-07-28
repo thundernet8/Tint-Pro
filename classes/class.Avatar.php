8fd60087cb6b0f3a9b4df0be58aacd410f681bdf
吴学谦 <chinash2010@gmail.com>
2016-07-28 21:21:35 +0800

    Migrate to PC

---
+++ classes/class.Avatar.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Copyright 2016, Zhiyanblog.com
+ * All right reserved.
+ *
+ * @author Zhiyan
+ * @date 16/6/24 14:37
+ * @license GPL v3 LICENSE
+ */
+ 
+?>
+<?php
+
+/**
+ * 用户头像
+ */
+
+class Avatar{
+    /**
+     * 构造器,根据用户id或邮箱获得头像
+     *
+     * @since   2.0.0
+     *
+     * @access  public
+     * @param   int     $uid    用户ID
+     * @param   string  $email  用户邮箱
+     */
+    public function __construct($uid, $email){
+
+    }
+}
