dceaa956fc482bb662f7efb4df94707198bea97e
吴学谦 <thundernet8@users.noreply.github.com>
2017-06-13 10:33:21 +0800

    Clean head

--- core/functions/func.Loader.php
+++ core/functions/func.Loader.php
@@ -1,18 +1,4 @@
 <?php
-/**
- * Copyright (c) 2014-2016, WebApproach.net
- * All right reserved.
- *
- * @since 2.0.0
- * @package Tint
- * @author Zhiyan
- * @date 2016/12/25 16:21
- * @license GPL v3 LICENSE
- * @license uri http://www.gnu.org/licenses/gpl-3.0.html
- * @link https://webapproach.net/tint.html
- */
-?>
-<?php
 
 /* 引入常量 */
 require_once 'Constants.php';
